71280b196c6f8d1b7b760ecf3901128fca869d9a
PhuHuy123 <nguyenphuhuy180920@gmail.com>
2022-07-25 10:27:48 +0700

    video 32/118 Create database

--- src/route/web.js
+++ src/route/web.js
@@ -1,13 +1,13 @@
 import express  from "express";
 import homeController from "../controllers/homeController";
 
+
 let router = express.Router();
 
 let initWebRoutes = (app)=>{
     router.get('/',homeController.getHomePage);
-    router.get('/it',(req, res)=>{
-        return res.send('Hello World with IT')
-    });
+    router.get('/crud',homeController.getCRUD);
+    router.post('/post-crud',homeController.postCRUD);
     return app.use("/", router);
 }
 module.exports = initWebRoutes;
