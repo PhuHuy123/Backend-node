40261409c94386feea261155b811618a55bfd14c
PhuHuy123 <nguyenphuhuy180920@gmail.com>
2022-07-26 22:40:19 +0700

    video 43/118 API login

---
+++ src/services/userService.js
@@ -0,0 +1,67 @@
+import db from '../models/index'
+import bcrypt from 'bcryptjs';
+const salt = bcrypt.genSaltSync(10);
+
+let handleUserLogin= (email, password) => {
+    return new Promise(async (resolve, reject) =>{
+         try {
+            let userData ={};
+
+            let isExist = await checkEmail(email);
+            if(isExist){        
+                let user = await db.User.findOne({
+                    where: {email: email},
+                    attributes: ['email', 'password', 'roleId'],
+                    raw: true,
+                })        
+                if(user){
+                    let checkPassword = await bcrypt.compareSync(password, user.password)
+
+                    if(checkPassword){
+                        userData.errCode = 0;
+                        userData.errMessage ="OK";
+                        delete user.password;
+                        userData.user=user;
+                    }
+                    else{
+                        userData.errCode = 3;
+                        userData.errMessage ="Password khong dung";
+                    }
+                }
+                else{
+                    userData.errCode = 2;
+                    userData.errMessage ="User khong ton tai"
+                }
+            }
+            else {
+                userData.errCode = 1;
+                userData.errMessage ="Email khong ton tai"
+            }
+            resolve(userData);
+        } catch (e) {
+            reject(e);
+        }
+     })
+}
+let checkEmail= (userEmail) => {
+    return new Promise(async (resolve, reject) =>{
+         try {
+            let user = await db.User.findOne({
+                where: {email: userEmail}
+            });
+            if(user){                
+                resolve(true);
+            }
+            else{
+                resolve(false);
+            }
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+     })
+}
+module.exports = {
+    handleUserLogin: handleUserLogin,
+
+}
